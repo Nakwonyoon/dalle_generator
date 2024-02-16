442ebc902c7517955f438b9b7488a4d13610ecf2
nakwonyoon <yoonnw1000@gmail.com>
2024-02-16 16:43:42 +0900

    adding ai img generator

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -14,4 +14,32 @@ router.route("/").get((req, res) => {
   res.send("Hello, world!");
 });
 
+router.route("/").post(async (req, res) => {
+  try {
+    // const aiResponse = await openai.createImage({
+    //
+    //   prompt,
+    //   n: 1,
+    //   size: "1024x1024",
+    // });
+    // image_url = response.data.data[0].url;
+    const { prompt } = req.body;
+    const aiResponse = await openai.images.generate({
+      model: "dall-e-2",
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.send(200).json({ photo: image });
+
+    console.log(image);
+  } catch (error) {
+    console.log("Error generating image", error);
+    res.status(500).send(error?.response.data.error.message || "Server error");
+  }
+});
+
 export default router;

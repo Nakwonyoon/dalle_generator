f507ddc83c8222733cd1b2463d52a0eb8480d5ca
윤낙원낙원 <sexy@MacBook-Pro.local>
2024-02-18 14:18:48 +0900

    finalized api key

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,26 +16,18 @@ router.route("/").get((req, res) => {
 
 router.route("/").post(async (req, res) => {
   try {
-    // const aiResponse = await openai.createImage({
-    //
-    //   prompt,
-    //   n: 1,
-    //   size: "1024x1024",
-    // });
-    // image_url = response.data.data[0].url;
     const { prompt } = req.body;
     const aiResponse = await openai.images.generate({
       model: "dall-e-2",
       prompt,
       n: 1,
-      size: "1024x1024",
+      size: "512x512",
       response_format: "b64_json",
     });
-    const image = aiResponse.data.data[0].b64_json;
-
-    res.send(200).json({ photo: image });
-
+    const image = aiResponse.data[0].b64_json;
     console.log(image);
+
+    res.status(200).json({ photo: image });
   } catch (error) {
     console.log("Error generating image", error);
     res.status(500).send(error?.response.data.error.message || "Server error");

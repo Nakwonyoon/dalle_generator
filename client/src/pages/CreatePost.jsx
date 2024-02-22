92459227e76a74492e8a842a5421d2f3e36449c5
nakwonyoon <yoonnw1000@gmail.com>
2024-02-22 10:38:31 +0900

    fix bug : wrong url provided

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -29,7 +29,8 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("https://localhost:3000/api/v1/dalle", {
+
+        const response = await fetch("http://localhost:3000/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -57,7 +58,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("https://localhost:3000/api/v1/posts", {
+        const response = await fetch("http://localhost:3000/api/v1/posts", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
